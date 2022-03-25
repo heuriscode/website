1174abb1b83d7095d273e5d13f7146f6373afd14
Tom Phung <tronglongphung@gmail.com>
2022-03-25 14:58:24 +1030

    remove layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 import '../styles/globals.css';
-import Layout from '@/components/Layout/Layout';
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
