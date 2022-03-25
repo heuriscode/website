42e6dedfe4c7d89db64bd83cdc9dd16273c6b26e
Long Phung <76096736+tronglongphung@users.noreply.github.com>
2022-03-25 14:59:11 +1030

    Add image (#2)
    * add image
    
    * add image
    
    * remove layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default function Home() {
-  return <div>add something in</div>;
+  return (
+    <img
+      className="flex items-center justify-center w-screen h-screen text-center"
+      src="/heuris-logo-vector.svg"
+      alt="heuris logo"
+    />
+  );
 }
