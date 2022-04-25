a93ed8ed515c1a5a7852cf9b7e6b5cf03277b5d9
Tom Phung <tronglongphung@gmail.com>
2022-04-25 13:53:29 +0930

    changed color

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -60,7 +60,7 @@ export default function Hero() {
         />
         <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
           <span className="block xl:inline">Data to enrich your</span>{' '}
-          <span className="block text-indigo-600 xl:inline">online business</span>
+          <span className="block text-green-600 xl:inline">online business</span>
         </h1>
         <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -70,7 +70,7 @@ export default function Hero() {
           <div className="rounded-md shadow">
             <a
               href="#"
-              className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-green-600 border border-transparent rounded-md hover:bg-green-700 md:py-4 md:text-lg md:px-10"
             >
               Get started
             </a>
@@ -78,7 +78,7 @@ export default function Hero() {
           <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
             <a
               href="#"
-              className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-600 bg-white border border-transparent rounded-md hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+              className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-green-600 bg-white border border-transparent rounded-md hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
             >
               Live demo
             </a>
