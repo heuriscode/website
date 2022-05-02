be515f7a972507db21677fead394c5ab945fe3e9
Tom Phung <tronglongphung@gmail.com>
2022-05-02 16:09:44 +0930

    resolve type issue

--- pages/work/[slug].tsx
+++ pages/work/[slug].tsx
@@ -93,20 +93,20 @@ export async function getStaticProps({ params }) {
 }
 
 const ptComponents = {
-  types: {
-    image: ({ value }) => {
-      if (!value?.asset?._ref) {
-        return null;
-      }
-      return (
-        <img
-          alt={value.alt || ' '}
-          loading="lazy"
-          src={urlFor(value).width(320).height(240).fit('max').auto('format')}
-        />
-      );
-    },
-  },
+  // types: {
+  //   image: ({ value }) => {
+  //     if (!value?.asset?._ref) {
+  //       return null;
+  //     }
+  //     return (
+  //       <img
+  //         alt={value.alt || ' '}
+  //         loading="lazy"
+  //         src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+  //       />
+  //     );
+  //   },
+  // },
 };
 
 export default PostLayout;
--- utils/sanityImage.ts
+++ utils/sanityImage.ts
@@ -1,6 +1,10 @@
 import imageUrlBuilder from '@sanity/image-url';
 import client from '@/client';
+import { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder';
 
-export function urlFor(source) {
+interface ImageProps extends Omit<React.HTMLProps<HTMLImageElement>, 'src'> {
+  src: string | ImageUrlBuilder;
+}
+export function urlFor(source: ImageProps) {
   return imageUrlBuilder(client).image(source);
 }
