3a598b39400b13806b68aad40938c9e5ed906901
Tom Phung <tronglongphung@gmail.com>
2022-05-04 00:51:29 +0930

    added redirect

--- next.config.js
+++ next.config.js
@@ -1,1 +1,6 @@
-module.exports = { reactStrictMode: true };
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [{ source: '/admin', destination: 'https://heuris-be.sanity.studio/', permanent: true }];
+  },
+};
