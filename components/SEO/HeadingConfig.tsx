4adb4d841941dab0b285957c9bed95d94669258b
Tom Phung <tronglongphung@gmail.com>
2022-05-07 00:22:37 +0930

    clean up

--- components/SEO/HeadingConfig.tsx
+++ components/SEO/HeadingConfig.tsx
@@ -1,11 +1,10 @@
 import { useRouter } from 'next/router';
-import { NextSeo, LocalBusinessJsonLd } from 'next-seo';
+import { NextSeo } from 'next-seo';
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function Header(props) {
   const router = useRouter();
-  const currentDomain = 'https://heuris.com';
+  const currentDomain = 'https://heuris.com.au';
   return (
     <>
       <Head>
@@ -27,46 +26,6 @@ export default function Header(props) {
         twitter={{
           cardType: 'summary_large_image',
         }}
-      />
-
-      <LocalBusinessJsonLd
-        type="LocalBusiness"
-        id={currentDomain}
-        name="Quoc Huy Technique"
-        description="Công ty TNHH Kỹ thuật Quốc Huy được thành lập từ năm 1999, có nhiều kinh nghiệm trong lĩnh vực kiểm tra không phá hủy (NDT), đo lường, thử nghiệm."
-        url="https://ndt-vn.com/contact"
-        telephone="+84 283 894 0623"
-        address={{
-          streetAddress: '209 Bạch Đằng',
-          addressLocality: 'P.3, Q. Gò Vấp',
-          addressRegion: 'HCM',
-          postalCode: '700000',
-          addressCountry: 'VN',
-        }}
-        geo={{
-          latitude: '10.815920677682236',
-          longitude: '106.67614333864016',
-        }}
-        images={['https://ndt-vn.com/logo.png']}
-        sameAs={[
-          'https://www.facebook.com/QuocHuyCompany',
-          'https://ndt-vn.com/',
-          'https://quochuy.com/',
-          'https://www.youtube.com/channel/UCDpuE4-ngw8vaOzQvx26wvg',
-        ]}
-        openingHours={[
-          {
-            opens: '08:00',
-            closes: '17:00',
-            dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-          },
-          {
-            opens: '8:00',
-            closes: '12:00',
-            dayOfWeek: 'Saturday',
-          },
-        ]}
-        priceRange="$$$$"
       />
     </>
   );
