b010288181869ce4c0b238f22d0c59a3b49b3200
Tom Phung <tronglongphung@gmail.com>
2022-05-07 14:29:05 +0930

    minor changes

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -18,7 +18,12 @@ export default function Portfolio({ work }) {
               <div className="flex flex-col justify-between flex-1 p-6 bg-white">
                 <div className="flex-1">
                   <a href={`/work/${post.slug.current}`} className="block">
-                    <p className="text-xl font-semibold text-gray-900 line-clamp-2 overflow-ellipsis">{post.title}</p>
+                    <p
+                      className="text-xl font-semibold text-gray-900 line-clamp-2 overflow-ellipsis"
+                      title={post.title}
+                    >
+                      {post.title}
+                    </p>
                     <p className="mt-3 text-base text-gray-500 line-clamp-2 overflow-ellipsis">{post.description}</p>
                   </a>
                   {post.categories && (
