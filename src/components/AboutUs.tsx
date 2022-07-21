35bc436a00dadeed99391ac4d3b79e4cf4edb95a
Jack McNicol <jackmcpickle@gmail.com>
2022-07-21 11:32:53 +0930

    update content

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,23 +4,23 @@ import Image from 'next/image';
 const supportLinks = [
   {
     name: 'Economic Analysis',
-    href: '#',
+    href: '/contact',
     description:
       'Leading data collection expertise including behavioural analysis and rapid assessment methodologies for impact evaluation.',
     icon: CashIcon,
   },
   {
     name: 'Strategy',
-    href: '#',
+    href: '/contact',
     description:
-      'Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.',
+      'Our strategy services help you develop a clear and actionable plan to achieve your business goals. We work with you to understand your unique situation and develop custom solutions that fit your needs.',
     icon: ChartSquareBarIcon,
   },
   {
     name: 'Tech Innovation',
-    href: '#',
+    href: '/contact',
     description:
-      'Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.',
+      'We help businesses and organizations innovate and stay ahead of the curve. We offer consulting, and development services to help you identify and implement a solution that best matches your needs.',
     icon: ChipIcon,
   },
 ];
@@ -29,21 +29,21 @@ export function AboutUsSection() {
   return (
     <section id="about" aria-labelledby="about-title" className="bg-white">
       {/* Header */}
-      <div className="relative pb-32 bg-gray-800">
+      <div className="relative bg-gray-800 pb-32">
         <div className="absolute inset-0">
           <Image
-            className="object-cover w-full h-full"
+            className="h-full w-full object-cover"
             src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
             alt=""
             layout="fill"
           />
           <div className="absolute inset-0 bg-charcoal-500 mix-blend-multiply" aria-hidden="true" />
         </div>
-        <div className="relative px-4 py-24 mx-auto max-w-7xl sm:py-32 sm:px-6 lg:px-8">
-          <h2 className="text-4xl font-extrabold tracking-tight text-white font-display md:text-5xl lg:text-6xl">
+        <div className="relative mx-auto max-w-7xl px-4 py-24 sm:py-32 sm:px-6 lg:px-8">
+          <h2 className="font-display text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
             At Heuris
           </h2>
-          <p className="max-w-3xl mt-6 text-xl text-gray-300">
+          <p className="mt-6 max-w-3xl text-xl text-gray-300">
             We use our knowledge and theory, underpinned by a creative mindset, to enabling the discovery of new and
             innovative solutions to the most complex problems.
           </p>
@@ -52,7 +52,7 @@ export function AboutUsSection() {
 
       {/* Overlapping cards */}
       <section
-        className="relative z-10 px-4 pb-32 mx-auto -mt-32 max-w-7xl sm:px-6 lg:px-8"
+        className="relative z-10 mx-auto -mt-32 max-w-7xl px-4 pb-32 sm:px-6 lg:px-8"
         aria-labelledby="contact-heading"
       >
         <h2 className="sr-only" id="contact-heading">
@@ -60,15 +60,15 @@ export function AboutUsSection() {
         </h2>
         <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8">
           {supportLinks.map(link => (
-            <div key={link.name} className="flex flex-col bg-white shadow-xl rounded-2xl">
+            <div key={link.name} className="flex flex-col rounded-2xl bg-white shadow-xl">
               <div className="relative flex-1 px-6 pt-16 pb-8 md:px-8">
-                <div className="absolute top-0 inline-block p-5 transform -translate-y-1/2 shadow-lg rounded-xl bg-primary-600">
-                  <link.icon className="w-6 h-6 text-white" aria-hidden="true" />
+                <div className="absolute top-0 inline-block -translate-y-1/2 transform rounded-xl bg-primary-600 p-5 shadow-lg">
+                  <link.icon className="h-6 w-6 text-white" aria-hidden="true" />
                 </div>
                 <h3 className="text-xl font-medium text-gray-900">{link.name}</h3>
                 <p className="mt-4 text-base text-gray-500">{link.description}</p>
               </div>
-              <div className="p-6 rounded-bl-2xl rounded-br-2xl bg-gray-50 md:px-8">
+              <div className="rounded-bl-2xl rounded-br-2xl bg-gray-50 p-6 md:px-8">
                 <a href={link.href} className="text-base font-medium text-primary-700 hover:text-primary-600">
                   Contact us<span aria-hidden="true"> &rarr;</span>
                 </a>
--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -9,35 +9,33 @@ const people = [
     role: 'Principal Economist',
     imageUrl: DanGregg,
     bio: 'Daniel has worked as a natural resource/agricultural economist and agribusiness analyst for Central Queensland University, Waikato University, the University of Adelaide and the University of New England applying various analytical techniques, including supply chain analysis, and productivity and efficiency analysis.',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    linkedinUrl: 'https://www.linkedin.com/in/daniel-gregg-a21b5919a/',
   },
   {
     name: 'Jack McNicol',
     role: 'Principal Architect / Developer',
     imageUrl: Jack,
-    bio: 'Jack is a Solutions Architect navigating enterprise-level Australian businesses, corporations and educational institutions through their digital transformation journey.',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    bio: 'Jack is a Solutions Architect who has worked on various enterprise-level Australian businesses, corporations and educational institutions through their digital transformation journey.',
+    twitterUrl: 'https://twitter.com/jackmcpickle',
+    linkedinUrl: 'https://www.linkedin.com/in/jackmcpickle/',
   },
   {
     name: 'Daniel Hill',
     role: 'Economist',
     imageUrl: DanHill,
     bio: 'Dan is an economist specialising in agriculture, rural development, water and natural resources, providing support for data analysis, research and policy advice.',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    linkedinUrl: 'https://www.linkedin.com/in/daniel-hill4/',
   },
 ];
 
 export function TeamSection() {
   return (
     <section id="team" aria-labelledby="team-title" className="bg-gray-100">
-      <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+      <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-12">
           <div className="space-y-5 sm:space-y-4">
-            <h2 className="text-3xl font-extrabold tracking-tight font-display sm:text-4xl">Our Team</h2>
-            <p className="text-xl text-gray-700">A focus on solutions and &apsot;doing&apsot;</p>
+            <h2 className="font-display text-3xl font-extrabold tracking-tight sm:text-4xl">Our Team</h2>
+            <p className="text-xl text-gray-700">A focus on solutions and &apos;doing&apos;</p>
             <p className="text-gray-500">
               Our team members balance innovation and research with an impact-driven focus. Achieving impactful change
               requires those that can act to do so by moving beyond theory to application. We act on the changes we see
@@ -56,7 +54,7 @@ export function TeamSection() {
                   <div className="space-y-4">
                     <div className="aspect-w-3 aspect-h-2">
                       <Image
-                        className="object-cover rounded-lg shadow-lg"
+                        className="rounded-lg object-cover shadow-lg"
                         layout="fill"
                         src={person.imageUrl}
                         alt={person.name}
@@ -71,18 +69,30 @@ export function TeamSection() {
                     </div>
 
                     <ul role="list" className="flex space-x-5">
+                      {person.twitterUrl && (
+                        <li>
+                          <a
+                            href={person.twitterUrl}
+                            target="_blank"
+                            className="text-gray-400 hover:text-gray-500"
+                            rel="noreferrer"
+                          >
+                            <span className="sr-only">Twitter</span>
+                            <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                              <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                            </svg>
+                          </a>
+                        </li>
+                      )}
                       <li>
-                        <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
-                          <span className="sr-only">Twitter</span>
-                          <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                          </svg>
-                        </a>
-                      </li>
-                      <li>
-                        <a href={person.linkedinUrl} className="text-gray-500 hover:text-gray-600">
+                        <a
+                          href={person.linkedinUrl}
+                          target="_blank"
+                          className="text-gray-500 hover:text-gray-600"
+                          rel="noreferrer"
+                        >
                           <span className="sr-only">LinkedIn</span>
-                          <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                          <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path
                               fillRule="evenodd"
                               d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
